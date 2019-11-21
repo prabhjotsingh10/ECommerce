19269e7812c5783ebaf0a340a0730b760f2be936
Prabhjot10 <prabhjot.singh232@gmail.com>
2019-11-20 20:47:37 -0500

    Added Buttons and Events for shirts- Second Commit

--- js/index.js
+++ js/index.js
@@ -1,40 +1,79 @@
-let quantity = parseInt(document.getElementById("quantity").innerHTML);
+/********VARIABLE DECLARATION *************/
+
+let quantity;
 let incBtn = document.getElementById("incrmnt");
 let dcrBtn = document.getElementById("dcrmnt");
+let incBtnFrShrts = document.getElementById("incrmntFrShrts");
+let dcrBtnFrShrts = document.getElementById("dcrmntFrShrts");
 let calcBtn = document.getElementById("Calculate");
-let unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML);
-let extndedPrice = parseInt(document.getElementById("extndpr").innerHTML);
-let newExtndPrce ;
+let unitPrice;
+let extndedPrice;
 
-function increment()
-{
-quantity+= 1;
-document.getElementById("quantity").innerHTML = quantity;
+//***********FUNCTION DECLARATION *********/
 
-}
-
-function decrement()
-{
-
-if(quantity<0){
-    alert("Come On Dude");
-    // document.getElementById("quantity").innerHTML = 0;
-}
-
-else
-{
-document.getElementById("quantity").innerHTML = quantity;
-quantity--;
-}
-}
-
-function extndPrice() {
+function increment() {
+    quantity = parseInt(document.getElementById("quantity").innerHTML);
+    quantity += 1;
+    document.getElementById("quantity").innerHTML = quantity;
     
-    newExtndPrce = unitPrice * extndedPrice;
-    console.log(extndedPrice);
+    unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML);
+    extndedPrice = unitPrice * quantity;
+    document.getElementById("extndpr").innerHTML = extndedPrice;
 }
 
+function decrement() {
 
-incBtn.addEventListener("click",increment);
+    quantity = parseInt(document.getElementById("quantity").innerHTML);
+    quantity--;
 
-dcrBtn.addEventListener("click",decrement);
+    if (quantity < 0) {
+        alert("Come On Dude");
+        // document.getElementById("quantity").innerHTML = 0;
+    }
+
+    else {
+        document.getElementById("quantity").innerHTML = quantity;
+
+        unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML);
+        extndedPrice = unitPrice * quantity;
+        document.getElementById("extndpr").innerHTML = extndedPrice
+    }
+}
+
+function incrementForShrts() {
+
+    quantity = parseInt(document.getElementById("quantityOfShrts").innerHTML);
+    quantity += 1;
+    document.getElementById("quantityOfShrts").innerHTML = quantity;
+    
+    unitPrice = parseFloat(document.getElementById("unitPriceForShrts").innerHTML);
+    extndedPrice = unitPrice * quantity;
+    document.getElementById("extndprForShrts").innerHTML = extndedPrice;
+}
+
+function decrementForShrts() {
+
+    quantity = parseInt(document.getElementById("quantityOfShrts").innerHTML);
+    quantity--;
+
+    if (quantity < 0) {
+        alert("Come On Dude");
+    }
+
+    else {
+        document.getElementById("quantityOfShrts").innerHTML = quantity;
+
+        unitPrice = parseFloat(document.getElementById("unitPriceForShrts").innerHTML)
+        extndedPrice = unitPrice * quantity;
+        document.getElementById("extndprForShrts").innerHTML = extndedPrice
+    }
+}
+
+//****************EXECUTION *********************/
+incBtn.addEventListener("click", increment);
+
+dcrBtn.addEventListener("click", decrement);
+
+incBtnFrShrts.addEventListener("click", incrementForShrts);
+
+dcrBtnFrShrts.addEventListener("click",decrementForShrts);
